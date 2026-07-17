c712777b3013a6b70378269fb1ae50c874fe14b1
Platunov <>
2026-07-17 14:30:24 +0500

    Support for Redmine 5

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'redmine'
 
-require_dependency 'that_hook'
+require_relative 'lib/that_hook'
 
 Rails.logger.info 'Starting That Email Log plugin for Redmine'
 
@@ -27,8 +27,8 @@ Redmine::Plugin.register :that_email_log do
     author 'Andriy Lesyuk for That Company'
     author_url 'http://www.andriylesyuk.com/'
     description 'Enables logging of all outgoing email messages that are sent by Redmine.'
-    url 'https://github.com/thatcompany/that_email_log'
-    version '0.0.4'
+    url 'https://github.com/KrissXan/that_email_log'
+    version '0.0.5'
 
     menu :admin_menu, :email_logs, :email_logs_path, :html => { :class => 'icon icon-email-logs' }, :before => :info
 
